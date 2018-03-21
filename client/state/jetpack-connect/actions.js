978a6a1c0c0e68fcd5bb1cd4f9adce670221067d
Jon Surrell <jon.surrell@automattic.com>
2018-03-21 14:01:11 +0100

    Add social sign up handling to Jetpack connect

--- client/state/jetpack-connect/actions.js
+++ client/state/jetpack-connect/actions.js
@@ -196,7 +196,7 @@ export function retryAuth( url, attemptNumber ) {
 	};
 }
 
-export function createAccount( userData ) {
+export function createAccount( userData, { service, access_token, id_token } = {} ) {
 	return dispatch => {
 		dispatch( recordTracksEvent( 'calypso_jpc_create_account', {} ) );
 
@@ -204,24 +204,65 @@ export function createAccount( userData ) {
 			type: JETPACK_CONNECT_CREATE_ACCOUNT,
 			userData: userData,
 		} );
-		wpcom.undocumented().usersNew( userData, ( error, data ) => {
-			if ( error ) {
-				dispatch(
-					recordTracksEvent( 'calypso_jpc_create_account_error', {
-						error_code: error.code,
-						error: JSON.stringify( error ),
-					} )
-				);
-			} else {
-				dispatch( recordTracksEvent( 'calypso_jpc_create_account_success', {} ) );
-			}
-			dispatch( {
-				type: JETPACK_CONNECT_CREATE_ACCOUNT_RECEIVE,
-				userData: userData,
-				data: data,
-				error: error,
+
+		if ( service ) {
+			// We're creating a new social account
+			wpcom.undocumented().usersSocialNew(
+				{
+					service,
+					access_token,
+					id_token,
+
+					/**
+					 * @TODO (sirreal) update to `jetpack-connect` when D11099-code lands
+					 *
+					 * The signup flow is required and affects some post signup activity.
+					 * `account` should be safe until patch lands.
+					 */
+					signup_flow_name: 'account',
+					// signup_flow_name: 'jetpack-connect',
+				},
+				( error, response ) => {
+					debug( 'Social error %o response %o', error, response );
+					if ( error ) {
+						dispatch(
+							recordTracksEvent( 'calypso_jpc_create_account_error', {
+								error_code: error.code,
+								error: JSON.stringify( error ),
+								social: true,
+							} )
+						);
+					} else {
+						dispatch( recordTracksEvent( 'calypso_jpc_create_account_success', { social: true } ) );
+					}
+					dispatch( {
+						type: JETPACK_CONNECT_CREATE_ACCOUNT_RECEIVE,
+						userData: { username: response.username },
+						data: { bearer_token: response.bearer_token },
+						error,
+					} );
+				}
+			);
+		} else {
+			wpcom.undocumented().usersNew( userData, ( error, data ) => {
+				if ( error ) {
+					dispatch(
+						recordTracksEvent( 'calypso_jpc_create_account_error', {
+							error_code: error.code,
+							error: JSON.stringify( error ),
+						} )
+					);
+				} else {
+					dispatch( recordTracksEvent( 'calypso_jpc_create_account_success' ) );
+				}
+				dispatch( {
+					type: JETPACK_CONNECT_CREATE_ACCOUNT_RECEIVE,
+					userData: userData,
+					data,
+					error,
+				} );
 			} );
-		} );
+		}
 	};
 }
 
